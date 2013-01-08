977359a2b81095ba294d5125a51a31b327627d15
Richard Lum <richardlum13@gmail.com>
2013-01-08 00:01:44 -0800

    partially added server listenging code

--- mtserver.c
+++ mtserver.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h> //atoi
+#include<sys/types.h>
+#include <sys/socket.h>
 
 void printhelpmessage(void)
 {
@@ -17,6 +19,59 @@ int valid(int connections, int port){
 }
 
 
+// based on beej's guide example server
+int get_main_socket(unsigned int port)
+{
+	int sockfd;
+	struct addrinfo hints,*servinfo	;
+	int rc ;
+
+	memset(&hints, 0, sizeof(hints);
+	hints.ai_family=AF_INET; //assume we only want ipv4
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if ((rc=getaddrinfo(NULL, port, &hints, &servinfo))!=0){
+		printf("getaddrinfo failed: %s\n", gai_strerror(rc));
+		return -1;
+	}
+	// servinfo is output - linked list of addresses we could bind to
+	
+	for (p = servinfo; p!=NULL; p=p->ai_next){
+		if ((sockfd=socket(p->ai_family, p->ai_socktype,p->ai_protocol))
+				== -1){
+			perror("server socket failed");
+			continue;  // this one didnt work, try next
+		}	
+		//SOL_SOCKET is level required to allow immediate socket reuse
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,&true, sizeof(int))
+				== -1){
+			perror("server setsocket failed");
+			continue;
+		}
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1){
+			close(sockfd);
+			perror("server bind failed");
+ 			continue;
+		}
+		//if we get here, we have a bound socket
+		break;
+	}
+	
+	if (p==NULL){
+		printf("server unable to bind to socket\n");
+		return -2;
+	}
+
+	freeaddrinfo(servinfo);
+
+	if(listen(sockfd, BACKLOG) == -1){
+		perror("listen");
+		return -1;
+	}
+	
+	
+}
 
 // should take in 2 parameters 
 // maxclients, server port number
@@ -35,9 +90,14 @@ if (!valid(max_connections, server_port)){
 	exit(EXIT_SUCCESS);
 }
 
-
-
-
+// create a socket
+/**
+int main_socket = socket (AF_INET, SOCK_STREAM, 0);
+if (main_socket < 0){
+	printf("Unable to assign main socket");
+	perror("main_socket");
+}
+**/
 
 
 
